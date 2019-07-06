21758bcd8c977efd4ab8290c85dd68a73a055acf
Saad E <21321555+se447@users.noreply.github.com>
2019-07-06 15:26:33 -0400

    Created method that returned an ArrayList with lengths of each String in original list

--- src/LengthsOfStrings.java
+++ src/LengthsOfStrings.java
@@ -1,19 +1,26 @@
+//Create the method lengths that gets a list of String variables as a parameter 
+//and returns an ArrayList that contains the lengths of the Strings in the same order as the original list.
+
 import java.util.ArrayList;
 
 public class LengthsOfStrings {
-    public static ArrayList<Integer> lengths(ArrayList<String> list) {
-        ArrayList<Integer> lengthList = new ArrayList<Integer>();
-        // write code here
+    public static ArrayList<Integer> lengths (ArrayList<String> list) {
+        ArrayList<Integer> listLength = new ArrayList<Integer>();
         
-        return lengthList;
+        for (String word : list) {
+            listLength.add(word.length());
+        }
+        
+        return listLength;
     }
-
-    public static void main(String[] args) {
+    
+    public static void main (String[] args) {
         ArrayList<String> list = new ArrayList<String>();
         list.add("Ciao");
         list.add("Moi");
         list.add("Benvenuto!");
         list.add("badger badger badger badger");
+        
         ArrayList<Integer> lengths = lengths(list);
         
         System.out.println("The lengths of the Strings: " + lengths);
